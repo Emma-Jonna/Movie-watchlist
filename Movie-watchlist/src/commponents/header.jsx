b1057c9bc94099f6e226ac2598ae72196d63f23d
Josef Forkman <strand.vatten@outlook.com>
2023-04-12 10:41:19 +0200

    add hedder

--- Movie-watchlist/src/commponents/header.jsx
+++ Movie-watchlist/src/commponents/header.jsx
@@ -1,5 +1,17 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+
+import styles from "../css/style.module.css";
 function Header () {
-    return <h1>Header works</h1>
+    return <header className={styles.Header}>
+        <img src="https://picsum.photos/60" alt="" />
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/Profil"><FontAwesomeIcon icon={faUser} size={"1x"} /></a></li>
+            </ul>
+        </nav>
+    </header>
 }
 
 export default Header
