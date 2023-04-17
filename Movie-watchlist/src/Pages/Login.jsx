1bb4334c887b6c9009ed88e24a946275ed99b49e
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-04-17 13:46:27 +0200

    can now get request token and access token

--- Movie-watchlist/src/Pages/Login.jsx
+++ Movie-watchlist/src/Pages/Login.jsx
@@ -1,7 +1,37 @@
 import React, {useState, useEffect} from "react";
+import {json, Navigate} from "react-router-dom";
 
 function Login() {
   const [requestToken, setrequestToken] = useState("");
+
+  if (window.sessionStorage.getItem("requestToken")) {
+    console.log(window.sessionStorage.getItem("requestToken"));
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json;charset=utf-8");
+    myHeaders.append(
+      "Authorization",
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGZmYTMwYjFjMWY2YzIzMWZkZDY2MDFjNjAxYWRkZiIsInN1YiI6IjY0MmQzOTk1NjQ3NjU0MDBkM2NhMGUyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DCq2hAOkHMO8FVFuCfDvr5gsHTjZ9QJV-a8h-u_2Pu4"
+    );
+
+    var raw = `{"request_token": "${window.sessionStorage.getItem(
+      "requestToken"
+    )}"}`;
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("https://api.themoviedb.org/4/auth/access_token", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        window.sessionStorage.setItem("accessToken", result.access_token);
+      })
+      .catch((error) => console.log("error", error));
+  }
 
   useEffect(() => {
     var myHeaders = new Headers();
@@ -24,17 +54,19 @@ function Login() {
     fetch("https://api.themoviedb.org/4/auth/request_token", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result.request_token);
         setrequestToken(result.request_token);
-        console.log(requestToken);
       })
       .catch((error) => console.log("error", error));
   }, []);
 
   let generateAccessToken = () => {
-    localStorage.setItem("requestToken", requestToken);
+    window.sessionStorage.setItem("requestToken", requestToken);
     window.location.href = `https://www.themoviedb.org/auth/access?request_token=${requestToken}`;
   };
+
+  if (window.sessionStorage.getItem("accessToken")) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div>
