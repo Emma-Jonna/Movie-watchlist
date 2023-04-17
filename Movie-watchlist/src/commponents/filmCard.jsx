4013cf108967db535c3aa1f3423ee0625c8f66eb
Josef Forkman <strand.vatten@outlook.com>
2023-04-17 11:25:08 +0200

    add better film

--- Movie-watchlist/src/commponents/filmCard.jsx
+++ Movie-watchlist/src/commponents/filmCard.jsx
@@ -1,10 +1,12 @@
+import styles from "../css/style.module.css";
+
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar} from "@fortawesome/free-solid-svg-icons";
 
 export function FilmCard({poster_path, title, id, vote_average}) {
     return (
-        <div>
-            <img src={"https://image.tmdb.org/t/p/w154/" + poster_path} alt="" />
+        <div className={styles.filmCard}>
+            <img src={"https://image.tmdb.org/t/p/w185/" + poster_path} alt="" />
             <h2>{title}</h2>
             <span>{vote_average}/10 <FontAwesomeIcon icon={faStar} /></span>
             <a href={"/Movie?id=" + id}>See more</a>
