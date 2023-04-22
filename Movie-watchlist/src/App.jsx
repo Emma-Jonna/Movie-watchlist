88339b91600d01d72c16e54563a1d7764827668a
JosefForkman <strand.vatten@outlook.com>
2023-04-22 15:44:02 +0100

    use usehooks-ts

--- Movie-watchlist/src/App.jsx
+++ Movie-watchlist/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import { createContext, useEffect, useState } from "react";
+
+import { useSessionStorage } from "usehooks-ts";
+
+import axios from "axios";
+
 import Header from "./commponents/header.jsx";
 import Home from "./Pages/Home.jsx";
 import Login from "./Pages/Login.jsx";
@@ -11,14 +16,23 @@ export const FavoriteContext = createContext();
 
 function App() {
   let [ Favorite, setFavorite ] = useState([])
+  const [session_id, setSession_id] = useSessionStorage('session_id', "");
 
   useEffect(() => {
-    const session_id = window.sessionStorage.getItem("session_id");
-      const urlFavorite = `https://api.themoviedb.org/3/account/{account_id}/watchlist/movies?api_key=${import.meta.env.VITE_TMDBv3}&session_id=${session_id}`;
-      fetch(urlFavorite)
-          .then(res => res.json())
-          .then(data => setFavorite(data.results));
-  }, [])
+      getWatchlist()
+  }, [session_id])
+
+  async function getWatchlist() {
+    if (session_id) {
+      const {data: { results }} = await axios.get('https://api.themoviedb.org/3/account/{account_id}/watchlist/movies', {
+        params: {
+          api_key: import.meta.env.VITE_TMDBv3,
+          session_id
+        }
+      })
+      setFavorite(results)
+    }
+  }
 
   return (
     <div>
