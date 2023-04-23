5fe72a9408aa57a1bd492bc0b8ee3cd0187c98e0
JosefForkman <strand.vatten@outlook.com>
2023-04-23 12:07:13 +0100

    add Hero

--- Movie-watchlist/src/Pages/Home.jsx
+++ Movie-watchlist/src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import {useEffect, useState} from "react";
 import styles from "../css/style.module.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import {FilmCard} from "../commponents/filmCard";
 
 function Home() {
   let [Films, setFilms] = useState([]);
+  const [Film, setFilm] = useState({});
 
   const films = async () => {
     const urlFilms = `https://api.themoviedb.org/3/movie/top_rated?api_key=${
@@ -19,6 +23,7 @@ function Home() {
       throw Error("Can´t get film");
     }
     setFilms(respondFilms.results);
+    setFilm(respondFilms.results[0])
   };
 
   const filter = () => {
@@ -28,6 +33,15 @@ function Home() {
     filter();
   }, []);
   return (
+    <>
+    <section className={styles.Hero}>
+      <img src={`https://image.tmdb.org/t/p/w1280/${Film.backdrop_path}`} alt="" />
+
+      <div className={styles.container}>
+        <h1>{Film.title}</h1>
+        <p>{Film.overview}</p>
+      </div>
+    </section>
     <div className={styles.HomeGrid}>
       {Films.map((item) => (
         <FilmCard
@@ -39,6 +53,7 @@ function Home() {
         />
       ))}
     </div>
+    </>
   );
 }
 
