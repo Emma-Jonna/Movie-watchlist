41f6c37a5c2731243ba1afd38c3758b59eff2181
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-05-01 10:49:02 +0200

    updated home to redirect to login if no session id

--- Movie-watchlist/src/Pages/Home.jsx
+++ Movie-watchlist/src/Pages/Home.jsx
@@ -1,54 +1,63 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import styles from "../css/style.module.css";
-
+import { Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import { FilmCard } from "../commponents/filmCard";
 import SearchForm from "../commponents/searchForm";
 
 function Home() {
-  let [Films, setFilms] = useState([]);
-  const [Film, setFilm] = useState({});
+    let [Films, setFilms] = useState([]);
+    const [Film, setFilm] = useState({});
 
-  const films = async () => {
-    const urlFilms = "https://api.themoviedb.org/3/movie/top_rated";
+    const navigate = useNavigate();
 
-    /* Fetch the movies */
-    const {data: { results }} = await axios.get(urlFilms, { params: { api_key: import.meta.env.VITE_TMDBv3 } });
+    const films = async () => {
+        const urlFilms = "https://api.themoviedb.org/3/movie/top_rated";
 
-    /* Set the state for the Films & Film form results */
-    setFilms(results);
-    setFilm(results[0])
-  };
-  
-  useEffect(() => {
-    films();
-  }, []);
-  
+        /* Fetch the movies */
+        const {
+            data: { results },
+        } = await axios.get(urlFilms, {
+            params: { api_key: import.meta.env.VITE_TMDBv3 },
+        });
 
-  return (
-    <>
-    <section className={styles.Hero}>
-      <img src={`https://image.tmdb.org/t/p/w1280/${Film.backdrop_path}`} alt="" />
+        /* Set the state for the Films & Film form results */
+        setFilms(results);
+        setFilm(results[0]);
+    };
 
-      <div className={styles.container}>
-        <h1>{Film.title}</h1>
-        <p>{Film.overview}</p>
-      </div>
-    </section>
+    useEffect(() => {
+        if (!sessionStorage.getItem("session_id")) {
+            navigate("/login");
+        } else {
+            films();
+        }
+    }, []);
 
-    <SearchForm setFilms={setFilms} setFilm={setFilm} />
+    return !Film.poster_path ? null : (
+        <>
+            <section className={styles.Hero}>
+                <img
+                    src={`https://image.tmdb.org/t/p/w1280/${Film.backdrop_path}`}
+                    alt=""
+                />
 
-    <section className={styles.HomeGrid}>
-      {Films.map((item) => (
-        <FilmCard
-          key={item.id}
-          Film={item}
-        />
-      ))}
-    </section>
-    </>
-  );
+                <div className={styles.container}>
+                    <h1>{Film.title}</h1>
+                    <p>{Film.overview}</p>
+                </div>
+            </section>
+
+            <SearchForm setFilms={setFilms} setFilm={setFilm} />
+
+            <section className={styles.HomeGrid}>
+                {Films.map((item) => (
+                    <FilmCard key={item.id} Film={item} />
+                ))}
+            </section>
+        </>
+    );
 }
 
 export default Home;
