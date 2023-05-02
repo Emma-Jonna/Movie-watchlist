d95db5f3fc818c0b743518c88acd19619ef84440
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-05-02 13:10:33 +0200

    added small fixes to move page

--- Movie-watchlist/src/commponents/filmCard.jsx
+++ Movie-watchlist/src/commponents/filmCard.jsx
@@ -9,42 +9,46 @@ import { Link } from "react-router-dom";
 import { FavoriteContext } from "../App";
 
 export function FilmCard({ Film }) {
-  let [Like, setLike] = useState(false);
+    let [Like, setLike] = useState(false);
 
-  let { Favorite, setFavorite } = useContext(FavoriteContext);
+    let { Favorite, setFavorite } = useContext(FavoriteContext);
 
-  async function getLike() {
-    const data = Favorite.filter((val) => {
-      return val.title == Film.title;
-    });
-    return data.length >= 1;
-  }
+    async function getLike() {
+        const data = Favorite.filter((val) => {
+            return val.title == Film.title;
+        });
+        return data.length >= 1;
+    }
 
-  useEffect(() => {
-    getLike()
-      .then((res) => {
-        setLike(res);
-      })
-      .catch((e) => console.error(e));
-  }, [Favorite]);
+    useEffect(() => {
+        getLike()
+            .then((res) => {
+                setLike(res);
+            })
+            .catch((e) => console.error(e));
+    }, [Favorite]);
 
-  // return <h1></h1>
-  return (
-    <div className={styles.filmCard}>
-      <img src={"https://image.tmdb.org/t/p/w185/" + Film.poster_path} alt="" />
-      <div className={styles.filmCardContiner}>
-        <h2>{Film.title}</h2>
+    // return <h1></h1>
+    return !Film.poster_path ? null : (
+        <div className={styles.filmCard}>
+            <img
+                src={"https://image.tmdb.org/t/p/w185/" + Film.poster_path}
+                alt=""
+            />
+            <div className={styles.filmCardContiner}>
+                <h2>{Film.title}</h2>
 
-        <div className={styles.rating}>
-          <span>
-            {Film.vote_average}/10 <FontAwesomeIcon icon={faStar} />
-          </span>
-          <LikeBtn Like={Like} setLike={setLike} Film={Film} />
+                <div className={styles.rating}>
+                    <span>
+                        {Math.round(Film.vote_average * 10) / 10}
+                        /10 <FontAwesomeIcon icon={faStar} />
+                    </span>
+                    <LikeBtn Like={Like} setLike={setLike} Film={Film} />
+                </div>
+                <Link className={styles.btn} to={"/Movie/" + Film.id}>
+                    See more
+                </Link>
+            </div>
         </div>
-        <Link className={styles.btn} to={"/Movie/" + Film.id}>
-          See more
-        </Link>
-      </div>
-    </div>
-  );
+    );
 }
